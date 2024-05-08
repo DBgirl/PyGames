a3e0be7055c763aee1ef1e2d4ad4c06d2435227b
uva <uvaranij.22@gmail.com>
2024-05-08 11:34:23 +0530

    updated code

--- RockPaperScissor-LizardSpock/rpssl.py
+++ RockPaperScissor-LizardSpock/rpssl.py
@@ -1,36 +1,52 @@
-from random import randint
+from random import choice
 
-#create a list of play options
-t = ["Rock", "Paper", "Scissors"]
+# create a list of play options
+plays = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
 
-#assign a random play to the computer
-computer = t[randint(0,2)]
+def get_verb(winner, loser):
+    verbs = {
+        "Rock": {"Scissors": "crushes", "Lizard": "crushes"},
+        "Paper": {"Rock": "covers", "Spock": "disproves"},
+        "Scissors": {"Paper": "cuts", "Lizard": "decapitates"},
+        "Lizard": {"Spock": "poisons", "Paper": "eats"},
+        "Spock": {"Rock": "vaporizes", "Scissors": "smashes"}
+    }
+    return verbs[winner][loser]
 
-#set player to False
-player = False
+while True:
+    computer = choice(plays) # assign a random play to the computer
 
-while player == False:
-#set player to True
-    player = input("Rock, Paper, Scissors?")
+    player = input("Rock, Paper, Scissors, Lizard, or Spock? (Type 'exit' to quit) ").capitalize()  # get player's choice
+    if player.lower() == 'exit':  # check if the player wants to exit
+        print("Thanks for playing!")
+        break
+
+    if player not in plays:  # check if the input is valid
+        print("That's not a valid play. Please choose Rock, Paper, Scissors, Lizard, or Spock.")
+        continue
+
     if player == computer:
         print("Tie!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
-        else:
-            print("You win!", player, "smashes", computer)
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
-        else:
-            print("You win!", player, "covers", computer)
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose...", computer, "smashes", player)
-        else:
-            print("You win!", player, "cut", computer)
+    elif (player == "Rock" and (computer == "Scissors" or computer == "Lizard")) \
+        or (player == "Paper" and (computer == "Rock" or computer == "Spock")) \
+        or (player == "Scissors" and (computer == "Paper" or computer == "Lizard")) \
+        or (player == "Lizard" and (computer == "Spock" or computer == "Paper")) \
+        or (player == "Spock" and (computer == "Rock" or computer == "Scissors")):
+        print(f"You win! {player} {get_verb(player, computer)} {computer}.")
     else:
-        print("That's not a valid play. Check your spelling!")
-    #player was set to True, but we want it to be False so the loop continues
-    player = False
-    computer = t[randint(0,2)]
+        print(f"You lose! {computer} {get_verb(computer, player)} {player}.") 
+    play_again = input("Do you want to play again? (yes/no) ").lower()
+    if play_again != 'yes':
+        print("Thanks for playing!")
+        break
+
+
+def get_verb(winner, loser):
+    verbs = {
+        "Rock": {"Scissors": "crushes", "Lizard": "crushes"},
+        "Paper": {"Rock": "covers", "Spock": "disproves"},
+        "Scissors": {"Paper": "cuts", "Lizard": "decapitates"},
+        "Lizard": {"Spock": "poisons", "Paper": "eats"},
+        "Spock": {"Rock": "vaporizes", "Scissors": "smashes"}
+    }
+    return verbs[winner][loser]
